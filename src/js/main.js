da2ce1cf6baef0c060285ef0b88274201344e80e
unknown <charles.chrismann@free.fr>
2022-10-06 21:55:44 +0200

    :art: IMPROVE 3dcss

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+a = 0.40
+b = 0.8
+c = 3
+
+w = 100
+h = 100
+
+// hauteur z en fonction de height
+th = b * h * c
+
+// hauteur z en fonction de w
+tw =  w *  c * b 
+// tw = a * w * ( c * b / a)
+
+console.log(th)
+console.log(tw)
